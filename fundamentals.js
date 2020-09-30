d903d2662512f9f0fcf76c471f84389f507f592e
Jack Pender <20085278@mail.wit.ie>
2020-09-30 17:24:55 +0100

    Arrays

--- fundamentals.js
+++ fundamentals.js
@@ -1,5 +1,13 @@
 const aCar = {
     owner : "Joe Bloggs",
+    // previous_owners: [
+    //     "Pat Smith - 1 Main Street",
+    //     "Sheila Dwyer - 2 High Street"
+    // ],
+    previous_owners: [
+        {name: "Pat Smith", address: "1 Main Street"},
+        {name: "Sheila Dwyer", address: "2 High Street"}
+    ],
     type : {
         make: "Toyota",
         model: "Corolla",
@@ -26,3 +34,5 @@ console.log("Reg = " + aCar.registration.year + "-" + aCar.registration.county +
 console.log("It is a " + aCar.color.exterior + " car, " 
 + aCar.mileage + " mileage, with " 
 + aCar.color.interior.texture + " interior.")
+console.log('First owner : ' + aCar.previous_owners[0] )
+console.log("First Owner: " + aCar.previous_owners[0].name + " - " + aCar.previous_owners[0].address)
