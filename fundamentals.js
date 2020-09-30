da51a7eb27674d92fce1c8f9978582cb01bb455d
Jack Pender <20085278@mail.wit.ie>
2020-09-30 17:13:51 +0100

    Dynamic Properties

--- fundamentals.js
+++ fundamentals.js
@@ -10,8 +10,19 @@ const aCar = {
         county: "WD",
         number: 1058
     },
+    mileage: 10000,
+    color: {
+        exterior: "red",
+        interior:{
+            texture: "leather",
+            shade: "cream"
+        }
+    }
 };
 
 console.log(aCar.owner);
 console.log(aCar.owner + ' drives a ' + aCar.type.make)
 console.log("Reg = " + aCar.registration.year + "-" + aCar.registration.county + "-" + aCar.registration.number)
+console.log("It is a " + aCar.color.exterior + " car, " 
++ aCar.mileage + " mileage, with " 
++ aCar.color.interior.texture + " interior.")
