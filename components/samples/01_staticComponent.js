eae33060caad7611c5106c12d1bd6be5464f3967
Jack Pender <20085278@mail.wit.ie>
2020-11-11 17:12:56 +0000

    Exercise 1 completed

--- components/samples/01_staticComponent.js
+++ components/samples/01_staticComponent.js
@@ -2,15 +2,56 @@ import React from "react";
 
 const Demo = () => {
   return (
-    <div>
-      <h1>Dynamic Languages</h1>
-      <ul>
-        <li>Python</li>
-        <li>Javascript</li>
-        <li>Ruby</li>
-        <li>PHP</li>
-      </ul>
-    </div>
+    // <div>
+    //   <h1>Dynamic Languages</h1>
+    //   <ul>
+    //     <li>Erlang</li>
+    //     <li>Clojure</li>
+    //     {/* <li>Ruby</li> */}
+    //     {/* <li>PHP</li> */}
+    //   </ul>
+    // </div>
+    <table className="table table-bordered">
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>No lectures</th>
+      <th>No practicals</th>
+    </tr>
+  </thead>
+  <tbody >
+    <tr>
+      <td>DevOps</td>
+      <td>2</td>
+      <td>2</td>
+    </tr>
+    <tr>
+      <td>Web App Development 2</td>
+      <td>3</td>
+      <td>2</td>
+    </tr>
+    <tr>
+      <td>NoSQL Databases</td>
+      <td>3</td>
+      <td>2</td>
+    </tr>
+    <tr>
+      <td>Embedded Systems Scripting</td>
+      <td>3</td>
+      <td>2</td>
+    </tr>
+    <tr>
+      <td>Project Semester 5</td>
+      <td>3</td>
+      <td>2</td>
+    </tr>
+    <tr>
+      <td>Measurement Systems</td>
+      <td>3</td>
+      <td>2</td>
+    </tr>
+  </tbody >
+</table>
   );
 };
 
